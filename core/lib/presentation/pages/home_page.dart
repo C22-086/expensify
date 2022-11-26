45654e96b76e3cd8938e88fb4959f68e7978b920
novannp <novangarut@gmail.com>
2022-11-26 10:46:47 +0700

    add appbar background

--- core/lib/presentation/pages/home_page.dart
+++ core/lib/presentation/pages/home_page.dart
@@ -7,9 +7,30 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-        body: Center(
-      child: Text('Home'),
+    return Scaffold(
+        body: Stack(
+      children: [
+        Container(
+          height: MediaQuery.of(context).size.height * 0.25,
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('assets/green_substract.png'),
+              fit: BoxFit.cover,
+            ),
+            color: Color(0xff1F8B4E),
+            borderRadius: BorderRadius.only(
+              bottomLeft: Radius.circular(50),
+            ),
+          ),
+          child: Row(children: [
+            Column(
+              children: [
+                Image.asset('assets/icon_wallet.png'),
+              ],
+            )
+          ]),
+        ),
+      ],
     ));
   }
 }
