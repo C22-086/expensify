6221d56a95da3326ec3c4354bc9b9138a1e6fcf1
novannp <novangarut@gmail.com>
2022-11-29 09:32:09 +0700

    fix onboarding dots indicator

--- core/lib/presentation/bloc/onboarding/onboarding_bloc.dart
+++ core/lib/presentation/bloc/onboarding/onboarding_bloc.dart
@@ -1,14 +1,10 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:equatable/equatable.dart';
 
-part 'onboarding_event.dart';
-part 'onboarding_state.dart';
-
-class OnboardingCubit extends Bloc<OnboardingEvent, OnboardingState> {
-  OnboardingCubit() : super(OnboardingState(0));
+class OnboardingCubit extends Cubit<int> {
+  OnboardingCubit(this.initialPage) : super(initialPage);
+  int initialPage = 0;
 
   void nextPage() {
-    // ignore: invalid_use_of_visible_for_testing_member
-    emit(OnboardingState(state.pageIndex + 1));
+    emit(state + 1);
   }
 }
